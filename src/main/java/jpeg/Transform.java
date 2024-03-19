25149f35bfbc753000dab4e72a432ba9e7be6ddb
OndraFoltyn <foltas.ondra@seznam.cz>
2024-03-19 23:30:28 +0100

    transform

--- src/main/java/jpeg/Transform.java
+++ src/main/java/jpeg/Transform.java
@@ -7,12 +7,13 @@ public class Transform {
     // Transformace předané barevné složky
     public static Matrix transform(Matrix input, TransformType type, int blockSize) {
         Matrix output = new Matrix(input.getRowDimension(), input.getColumnDimension());
-        Matrix A = getTransformMatrix(type, blockSize);     // transformacni matice
-        Matrix aTranspose = A.transpose();                  // transponovana transformacni matice
 
         for (int i = 0; i < input.getRowDimension(); i += blockSize) {
             for (int j = 0; j < input.getColumnDimension(); j += blockSize) {
+
                 Matrix X = input.getMatrix(i, i + blockSize - 1, j, j + blockSize - 1);
+                Matrix A = getTransformMatrix(type, X.getRowDimension());       // transformacni matice
+                Matrix aTranspose = A.transpose();                              // transponovana transformacni matice
                 Matrix mat = A.times(X).times(aTranspose);
                 output.setMatrix(i, i + blockSize - 1, j, j + blockSize - 1, mat);
 
@@ -24,14 +25,16 @@ public class Transform {
     // Inverzní transformace
     public static Matrix inverseTransform(Matrix input, TransformType type, int blockSize) {
         Matrix output = new Matrix(input.getRowDimension(), input.getColumnDimension());
-        Matrix A = getTransformMatrix(type, blockSize);     // transformacni matice
-        Matrix aTranspose = A.transpose();                  // transponovana transformacni matice
 
         for (int i = 0; i < input.getRowDimension(); i += blockSize) {
             for (int j = 0; j < input.getColumnDimension(); j += blockSize) {
+
                 Matrix O = input.getMatrix(i, i + blockSize - 1, j, j + blockSize - 1);
+                Matrix A = getTransformMatrix(type, O.getRowDimension());       // transformacni matice
+                Matrix aTranspose = A.transpose();                              // transponovana transformacni matice
                 Matrix mat = aTranspose.times(O).times(A);
                 output.setMatrix(i, i + blockSize - 1, j, j + blockSize - 1, mat);
+
             }
         }
         return output;
@@ -39,36 +42,41 @@ public class Transform {
 
     // Získání transformační matice, generované podle typu a velikosti bloku
     public static Matrix getTransformMatrix(TransformType type, int blockSize) {
-        Matrix transformMatrix = new Matrix(blockSize, blockSize);
-
         switch (type) {
             case DCT:
-                for (int i = 0; i < blockSize; i++) {
-                    for (int j = 0; j < blockSize; j++) {
-                        double C = Math.cos(((2 * j + 1) * i * Math.PI) / (2 * blockSize));
-                        if (i == 0) {
-                            C /= Math.sqrt(blockSize);
-                        } else {
-                            C *= Math.sqrt((double) 2 / blockSize);
-                        }
-                        transformMatrix.set(i, j, C);
-                    }
-                }
-                break;
+                return getDCTMatrix(blockSize);
 
             case WHT:
+                Matrix transformMatrix;
                 int N = 1;
                 while (N < blockSize) {
                     N *= 2;
                 }
                 Matrix Hadamard = generateHadamard(N);
                 transformMatrix = Hadamard.times(1 / Math.sqrt(blockSize));
-                break;
+                return transformMatrix;
+
+            default:
+                return Matrix.random(1,1);
         }
-        return transformMatrix;
     }
 
-    // Generate the Hadamard matrix
+    private static Matrix getDCTMatrix(int blockSize) {
+        Matrix dctMatrix = new Matrix(blockSize, blockSize);
+        for (int i = 0; i < blockSize; i++) {
+            for (int j = 0; j < blockSize; j++) {
+                double C = Math.cos((Math.PI * (2 * j + 1) * i) / (2 * blockSize));
+                if (i == 0) {
+                    C = Math.sqrt((double) 1 / blockSize) * C;
+                } else {
+                    C = Math.sqrt((double) 2 / blockSize) * C;
+                }
+                dctMatrix.set(i, j, C);
+            }
+        }
+        return dctMatrix;
+    }
+
     public static Matrix generateHadamard(int size) {
         Matrix hadamard = new Matrix(size, size);
         hadamard.set(0, 0, 1);
@@ -82,5 +90,8 @@ public class Transform {
             }
         }
         return hadamard;
+
+
     }
 }
+
